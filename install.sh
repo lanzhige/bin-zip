cb55e239f4f218adb64954333da59d645a37708f
Lei Zhang (Student) <lzhan253@sundevils.asu.edu>
2018-08-24 16:37:28 -0700

    fix bash

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ cmake ..
 cmake --build . --config Release
 cd ..
 cd ..
-cp ./bin-zip/build/src/Release/bin-zip.exe ./
+cp ./bin-zip/build/src/Release/binzip.exe ./
 cp ./bin-zip/bin/zlib.dll ./
+cp ./bin-zip/config.ini ./
 cp ./bin-zip/README.md ./
 rm -rf ./bin-zip
